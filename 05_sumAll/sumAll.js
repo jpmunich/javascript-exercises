ecb8c56372d739c0658093301689aa6e7fa90c27
jpmunich <parkercoyle8@gmail.com>
2023-07-07 15:41:43 -0400

    Exercise 05 Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    let total = 0;
+    const range = Math.abs(max - min);
+
+    for (let i = 0; i <= range; i++) {
+        total += min;
+        if (min > max) min--;
+        else min++;
+    }
+    return total;
 };
 
+console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
