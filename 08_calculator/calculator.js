2fe1f10af16b04a9650e7e4c9d9505e6172bb371
jpmunich <parkercoyle8@gmail.com>
2023-07-09 15:42:14 -0400

    Exercise 08 Solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, addend) => sum + addend, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...x) {
+  let product = 1; 
+  x.forEach(multiple => {product *= multiple});
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, power) {
+	return Math.pow(x, power);
+}
+
+const factorial = function(x) {
+  if (x === 0) return 1;
+  let product = x;
+  const i = 1;
+
+	while (i < x) {
+    x--;
+    product *= x;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
-};
+console.log(factorial(4))
 
 // Do not edit below this line
 module.exports = {
