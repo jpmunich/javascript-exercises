0781517798a020f18a690f3bef5f79b40d7e55ab
jpmunich <parkercoyle8@gmail.com>
2023-07-10 01:55:31 -0400

    Exercise 10 Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    previous = 0;
+    total = 1;
+
+    for (let i = 1; i < parseInt(x); i++) {
+        total += previous;
+        previous = total - previous
+    }
+    return total;
 
 };
 
+console.log(fibonacci(0));
 // Do not edit below this line
 module.exports = fibonacci;
