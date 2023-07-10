4385f443fdada3d62d11465447d0f7c5f923e11b
jpmunich <parkercoyle8@gmail.com>
2023-07-10 01:56:22 -0400

    Exercise 12 in progress

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const newArr = arr.sort((a, b) => {
+        if (a.yearOfBirth > b.yearOfBirth) {return 1;}
+        else {return -1;}
+    }).filter((item, i) => {return item.yearOfDeath !== undefined || i === 0})
+    return newArr[0];
 };
 
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]))
 // Do not edit below this line
 module.exports = findTheOldest;
