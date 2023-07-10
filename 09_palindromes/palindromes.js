aaa1b90688ac585eb1cf6309e07518f7591780e2
jpmunich <parkercoyle8@gmail.com>
2023-07-10 01:55:18 -0400

    Exercise 09 Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/ /g, "").toLowerCase().split('').reverse().join('') === word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ /g, "").toLowerCase();
 };
 
 // Do not edit below this line
